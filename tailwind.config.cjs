5b9f561ff5b5492e831a4da7aa5df22c64a5db83
Daniel Segarra <danielsegarra36@gmail.com>
2024-03-13 11:11:30 -0400

    init basic home layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        // heading: "rgb(142, 124, 68)",
+        heading: "rgb(74, 121, 161)",
+        cohortHeader: "#7291ac",
+        cohortHeading: "burlywood",
+        buttonText: "#0c4e88",
+        buttonHover: "#a8793c",
+        color: "#fff6e4",
+      },
+    },
+  },
+  plugins: [],
 };
